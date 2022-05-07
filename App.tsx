b6769692ebe4669518f55e9a1509c628a86c1687
Pedro Davila <pedrodavila.bovolin@gmail.com>
2022-05-07 16:44:39 -0300

    feat: added theme config

--- App.tsx
+++ App.tsx
@@ -1,8 +1,40 @@
 import React from 'react';
+
+import { useFonts } from 'expo-font'
+import AppLoading from 'expo-app-loading';
+
+import {
+  Inter_400Regular,
+  Inter_500Medium
+} from '@expo-google-fonts/inter'
+
+import {
+  Archivo_400Regular,
+  Archivo_500Medium,
+  Archivo_600SemiBold
+} from '@expo-google-fonts/archivo'
+
+import { ThemeProvider } from 'styled-components';
+import theme from './src/styles/theme';
+
 import { Home } from './src/screens/Home';
 
 export default function App() {
+  const [fontsLoaded] = useFonts({
+    Inter_400Regular,
+    Inter_500Medium,
+    Archivo_400Regular,
+    Archivo_500Medium,
+    Archivo_600SemiBold
+  })
+
+  if (!fontsLoaded) {
+    return <AppLoading />
+  }
+
   return (
-    <Home />
+    <ThemeProvider theme={theme}>
+      <Home />
+    </ThemeProvider>
   );
 }
