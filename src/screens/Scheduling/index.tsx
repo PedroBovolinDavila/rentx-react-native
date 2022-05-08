59512513af4cdb3e53417bf3a1e16334d38f0ba4
Pedro Davila <pedrodavila.bovolin@gmail.com>
2022-05-08 20:28:32 -0300

    feat: added scheduling screen

--- src/screens/Scheduling/index.tsx
+++ src/screens/Scheduling/index.tsx
@@ -1,6 +1,10 @@
 import React from "react";
 
 import { BackButton } from "../../components/BackButton";
+import { Button } from "../../components/Button";
+import { Calendar } from "../../components/Calendar";
+
+import ArrowSvg from '../../assets/arrow.svg'
 
 import { useTheme } from "styled-components";
 
@@ -9,13 +13,26 @@ import {
   Header,
   BackButtonWrapper,
   Title,
+  RentalPeriod,
+  DateInfo,
+  DateTitle,
+  DateValue,
+  Content,
+  Footer
 } from './styles'
+import { StatusBar } from "react-native";
 
 export function Scheduling() {
   const theme = useTheme()
 
   return (
     <Container>
+      <StatusBar
+        barStyle="light-content"
+        backgroundColor="transparent"
+        translucent
+      />
+
       <Header>
         <BackButtonWrapper>
           <BackButton onPress={() => { }} color={theme.colors.shape} />
@@ -23,10 +40,36 @@ export function Scheduling() {
 
         <Title>
           Escolha uma {'\n'}
-          Data de inicio e {'\n'}
+          data de inicio e {'\n'}
           fim do aluguel
         </Title>
+
+        <RentalPeriod>
+          <DateInfo>
+            <DateTitle>DE</DateTitle>
+            <DateValue selected={false}>
+              18/06/2021
+            </DateValue>
+          </DateInfo>
+
+          <ArrowSvg />
+
+          <DateInfo>
+            <DateTitle>ATÉ</DateTitle>
+            <DateValue selected={false}>
+              18/06/2021
+            </DateValue>
+          </DateInfo>
+        </RentalPeriod>
       </Header>
-    </Container>
+
+      <Content>
+        <Calendar />
+      </Content>
+
+      <Footer>
+        <Button title="Confirmar" />
+      </Footer>
+    </Container >
   )
 }
--- src/screens/Scheduling/styles.ts
+++ src/screens/Scheduling/styles.ts
@@ -1,6 +1,10 @@
-import styled from "styled-components/native";
-import { getStatusBarHeight } from "react-native-iphone-x-helper";
+import styled, { css } from "styled-components/native";
 import { RFValue } from "react-native-responsive-fontsize";
+import { getStatusBarHeight } from "react-native-iphone-x-helper";
+
+interface IDateValueProps {
+  selected: boolean;
+}
 
 export const Container = styled.View`
   flex: 1;
@@ -9,10 +13,11 @@ export const Container = styled.View`
 
 export const Header = styled.View`
   width: 100%;
-  height: 325px;
+  height: 350px;
   background-color: ${({ theme }) => theme.colors.header};
   justify-content: center;
   padding: 25px;
+  padding-top: ${getStatusBarHeight() + 30}px;
 `;
 
 export const BackButtonWrapper = styled.View`
@@ -24,4 +29,47 @@ export const Title = styled.Text`
   font-family: ${({ theme }) => theme.fonts.secondary_600};
   font-size: ${RFValue(34)}px;
   margin-top: 24px;
+`;
+
+export const RentalPeriod = styled.View`
+  width: 100%;
+  flex-direction: row;
+  justify-content: space-around;
+  align-items: center;
+  margin: 32px 0;
+`;
+
+export const DateInfo = styled.View`
+  width: 30%;
+`;
+
+export const DateTitle = styled.Text`
+  color: ${({ theme }) => theme.colors.text};
+  font-family: ${({ theme }) => theme.fonts.secondary_500};
+  font-size: ${RFValue(10)}px;
+`;
+
+export const DateValue = styled.Text<IDateValueProps>`
+  color: ${({ theme }) => theme.colors.shape};
+  font-family: ${({ theme }) => theme.fonts.primary_500};
+  font-size: ${RFValue(15)}px;
+
+  ${({ selected, theme }) =>
+    !selected &&
+    css`
+      border-bottom-width: 1px;
+      border-bottom-color: ${theme.colors.text};
+      padding-bottom: 5px;
+    `}
+`;
+
+export const Content = styled.ScrollView.attrs({
+  contentContainerStyle: {
+    paddingBottom: 24,
+  },
+  showVerticalScrollIndicator: false,
+})``;
+
+export const Footer = styled.View`
+  padding: 24px;
 `;
