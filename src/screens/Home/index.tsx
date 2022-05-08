7f24edb73d9dc2b3af9de43ce077610c196da912
Pedro Davila <pedrodavila.bovolin@gmail.com>
2022-05-07 23:40:23 -0300

    feat: added car card

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -2,6 +2,8 @@ import React from "react";
 import { StatusBar } from "react-native";
 import { RFValue } from "react-native-responsive-fontsize";
 
+import { Car } from "../../components/Car";
+
 import Logo from '../../assets/logo.svg'
 
 import {
@@ -12,6 +14,26 @@ import {
 } from './styles'
 
 export function Home() {
+  const carDataOne = {
+    brand: "Audi",
+    name: "RS 5 Coupé",
+    rent: {
+      period: 'Ao dia',
+      price: 120
+    },
+    thumbnail: "https://www.pngplay.com/wp-content/uploads/13/Audi-RS5-PNG-HD-Quality.png"
+  }
+
+  const carDataTwo = {
+    brand: "Porshe",
+    name: "Panamera",
+    rent: {
+      period: 'Ao dia',
+      price: 340
+    },
+    thumbnail: "https://cdn.jdpower.com/ChromeImageGallery/Expanded/White/640/2016PRC010009_640/2016PRC010009_640_01.jpg"
+  }
+
   return (
     <Container>
       <StatusBar
@@ -31,6 +53,9 @@ export function Home() {
           </TotalCars>
         </HeaderContent>
       </Header>
+
+      <Car data={carDataOne} />
+      <Car data={carDataTwo} />
     </Container>
   )
 }
